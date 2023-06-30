d8ffeed1499caab1d80f1892d187accb55484ced
Ayush Singh <mr.technical19@gmail.com>
2023-06-30 19:47:31 +0530

    addedScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -7,10 +12,22 @@ import TopWalls from "./components/TopWalls";
 import WallpaperPage from "./components/WallpaperPage";
 import Random from "./components/Random";
 import SearchData from "./components/SearchData";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
