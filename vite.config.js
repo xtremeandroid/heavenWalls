59a8924a3165c78cbee9b931e0d0da6c580c23c4
Ayush Singh <mr.technical19@gmail.com>
2023-09-15 19:12:44 +0530

    noProxyVite

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://heaven-walls-api.vercel.app",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
 });
